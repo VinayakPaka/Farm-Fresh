a1c6483d52e5a964da10826b602d9780c38f1b77
Vinayak Paka <vinayakpaka22@gmail.com>
2025-11-12 23:27:11 +0530

    async storage & save token

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,23 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import asyncStorage from '@react-native-async-storage/async-storage';
 
+
+
 const API_BASE_URL = 'http://10.51.14.91:3000/';
+
+export const getToken = () => {
+
+};
+
+export const saveToken = async() => {
+    try {
+        const token = await AsyncStorage.setItem('authToken', token)
+        return token
+    } catch (error) {
+        console.error("Token cannot be Saved");
+        return null
+    }
+}; 
 
 const apiRequest = async (endpoint,
     method = "GET",
@@ -31,6 +48,10 @@ const apiRequest = async (endpoint,
 
 export const authApi = {
     signup: async (name, email, password) => {
+        return apiRequest('/api/auth/signup', "POST", {name, email, password})
+    },
 
-    }
-} 
+    login: async (email, password) => {
+        return apiRequest('/api/auth/signup', "POST", {email, password})
+    },
+} ;
